cb0080c63a33eee5685fda8978298dfc04967adb
Nikolay Kovyrshin <nikolaykovyrshin@outlook.com>
2024-01-25 21:44:02 +0300

    configurate admin

--- WebBooks/catalog/models.py
+++ WebBooks/catalog/models.py
@@ -62,6 +62,12 @@ class Book(models.Model):
     def get_absolute_url(self):
         return reverse('book-detail', args=[str(self.pk)])
 
+    def display_author(self):
+        return ', '.join([author.last_name for author in
+                          self.author.all()])
+
+    display_author.short_description = 'Авторы'
+
 
 class Status(models.Model):
     name = models.CharField(max_length=20,
