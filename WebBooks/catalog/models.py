06027d2d01431e858c40fb2093c5bd40d568bf2b
Nikolay Kovyrshin <nikolaykovyrshin@outlook.com>
2024-02-02 17:33:55 +0300

    added auth

--- WebBooks/catalog/models.py
+++ WebBooks/catalog/models.py
@@ -1,3 +1,6 @@
+from datetime import date
+
+from django.contrib.auth.models import User
 from django.db import models
 from django.urls import reverse
 
@@ -93,9 +96,17 @@ class BookInstance(models.Model):
     due_back = models.DateField(null=True, blank=True,
                                 help_text="Введите конец срока статуса",
                                 verbose_name="Дата окончания статуса")
+    borrower = models.ForeignKey(User, on_delete=models.SET_NULL,
+                                 null=True, blank=True,
+                                 verbose_name="Заказчик",
+                                 help_text='Выберите заказчика книги')
 
     class Meta:
         ordering = ["-due_back"]
+
+    @property
+    def is_overdue(self):
+        return self.due_back and date.today() > self.due_back
 
     def __str__(self):
         return '%s %s %s' % (self.inv_nom, self.book, self.status)
--- WebBooks/catalog/views.py
+++ WebBooks/catalog/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.views import generic
 
@@ -34,3 +35,15 @@ class AuthorListView(generic.ListView):
 
 class BookDetailView(generic.DetailView):
     model = Book
+
+
+class LoanedBookByUserListView(LoginRequiredMixin, generic.ListView):
+    model = BookInstance
+    template_name = 'catalog/bookinstance_list_borrowed_user.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return (BookInstance.objects
+                .filter(borrower=self.request.user)
+                .filter(status='2')
+                .order_by('due_back'))
